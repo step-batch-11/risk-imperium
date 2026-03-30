48fc0b22733d9316303af82a7dc0abfa3b7df104
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2026-03-30 10:13:55 +0530

    Removes unused and commented out code

--- public/scripts/features/setup.js
+++
@@ -1,3 +0,0 @@
-export const setup = () => {
-  console.log("Setup in progress");
-}
--- public/scripts/server_calls.js
+++
@@ -1,6 +0,0 @@
-// export const sendSomeData = (api, data) => {
-//   fetch(api, {
-//     method: "POST",
-//     body: JSON.stringify(data)
-//   })
-// }
